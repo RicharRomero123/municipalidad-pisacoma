6e83797aa05025b276f80604fcb1eee1f0768555
RicharRomero123 <89022073+RicharRomero123@users.noreply.github.com>
2024-07-23 01:43:56 -0500

    update sider bar color

--- src/components/Siderbar/SiderbarInstitucional.tsx
+++ src/components/Siderbar/SiderbarInstitucional.tsx
@@ -7,17 +7,17 @@ const SidebarInstitucional = () => {
             <h2 className="text-green-700 font-bold mb-4">Institucional</h2>
             <ul className="space-y-2">
                 <li>
-                    <Link href="/institucional/alcalde" className="text-green-700 font-bold block px-4 py-2 bg-green-100 rounded-md">
+                    <Link href="/institucional/alcalde" className="text-blue-500 font-bold block px-4 py-2 hover:bg-blue-100 rounded-md">
                     Alcalde
                     </Link>
                 </li>
                 <li>
-                    <Link href="/institucional/organigrama" className="text-gray-700 block px-4 py-2 hover:bg-green-100 rounded-md">
+                    <Link href="/institucional/organigrama" className="text-gray-700 block px-4 py-2 hover:bg-blue-100 rounded-md">
                     Organigrama 
                     </Link>
                 </li>
                 <li>
-                    <Link href="/institucional/concejo-municipal" className="text-gray-700 block px-4 py-2 hover:bg-green-100 rounded-md">
+                    <Link href="/institucional/concejo-municipal" className="text-gray-700 block px-4 py-2 hover:bg-blue-100 rounded-md">
                     Concejo Municipal 
                     </Link>
                 </li>
